d6bbb18d942d4b570a53a7dff9a5aa519183c2d1
Jian Zhang <jzhangn@amazon.com>
2019-01-11 13:37:41 -0800

    One-To-Many Category

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -110,7 +110,7 @@ class EventsController < ApplicationController
 
 
     def event_params
-        params.require(:event).permit(:name, :description)
+        params.require(:event).permit(:name, :description, :category_id)
     end
 
     private :event_params
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ApplicationRecord
     validates_presence_of :name
     has_many :attendees
+    belongs_to :category
+
+    delegate :name, :to => :category, :prefix => true, :allow_nil => true
 end
